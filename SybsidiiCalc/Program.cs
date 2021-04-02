7aa5d7408fb802f238be66f5dbc692de21061a0f
LordAids <62069378+LordAids@users.noreply.github.com>
2021-04-02 18:01:37 +0300

    Initial commit

---
+++ SybsidiiCalc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SybsidiiCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SybsidiiCalc/Volume.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// Объём
+    /// </summary>
+    public class Volume
+    {
+        /// <summary>
+        /// Идентификатор услуги
+        /// </summary>
+        public int ServiceId { get; set; }
+        
+        /// <summary>
+        /// Идентификатор дома
+        /// </summary>
+        public int HouseId { get; set; }
+        
+        /// <summary>
+        /// Месяц начисления
+        /// </summary>
+        public DateTime Month { get; set; }
+        
+        /// <summary>
+        /// Значение объёма
+        /// </summary>
+        public decimal Value { get; set; }
+
+       
+       
+    }
+}
